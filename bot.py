c00948adebfe79606f62dfda3c210fba4ee0f6e3
Denys Romaniuk <dionysius.stealth@gmail.com>
2024-12-07 17:23:52 +0100

    Dummy changes to test pylint

--- bot.py
+++ bot.py
@@ -3,5 +3,6 @@
 import logging
 import sys
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
+if __name__ == "__main__":   
+    logging.basicConfig(   level=logging.DEBUG, stream=sys.stderr)
+
